46575533a95c606c07a9bf59efd5763efa11b480
Fernando Paschualetto <paschualetto@gmail.com>
2015-08-20 21:44:36 -0300

    classe app não está indo

--- src/main/java/br/com/fafidev/webteca/App.java
+++ src/main/java/br/com/fafidev/webteca/App.java
@@ -13,7 +13,7 @@ public class App {
     public static void main(String[] args) {
         EntityManager em = Conexao.getEntityManager();
 
-        Usuario us = new Usuario("blablabla", "senha10");
+        Usuario us = new Usuario("fernando", "senha10");
         em.getTransaction().begin();
         em.persist(us);
         em.getTransaction().commit();
